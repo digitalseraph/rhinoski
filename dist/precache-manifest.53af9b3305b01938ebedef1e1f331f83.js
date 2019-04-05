214178a64f3a1dd71e841f5920faac88ab6295e0
David Miller <davidmiller.avatar@gmail.com>
2019-04-05 15:20:34 -0400

    untracked files on feature/doc-and-testing: 34fa200 REFACTOR(src): Major refactor of code base

---
+++ dist/precache-manifest.53af9b3305b01938ebedef1e1f331f83.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "68e0b4d91ba53155e7acd92894c24b20",
+    "url": "/index.html"
+  },
+  {
+    "url": "/assets/js/vendors.abb731d2a6b7454e107d.bundle.js"
+  },
+  {
+    "url": "/assets/js/runtime.abb731d2a6b7454e107d.bundle.js"
+  },
+  {
+    "url": "/assets/js/app.abb731d2a6b7454e107d.bundle.js"
+  },
+  {
+    "url": "/assets/css/0b2d9d882f4927f9b2b8.css"
+  }
+];
